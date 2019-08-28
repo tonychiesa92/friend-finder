0da6b6998de94302b5f8bd007021e53db7824607
Tony C <tonychiesa92@gmail.com>
2019-08-28 19:24:14 -0400

    created friends .js that has inital array of objects

---
+++ app/data/friends.js
@@ -0,0 +1,33 @@
+var friends = [
+    {
+        "name": "Ahmed",
+        "photo": "https://picsum.photos/id/435/200/300",
+        "scores": ["5", "1", "4", "4", "5", "1", "2", "5", "4", "1"]
+    },
+    {
+        "name": "Jacob Deming",
+        "photo": "https://picsum.photos/id/436/200/300",
+        "scores": ["4", "2", "5", "1", "3", "2", "2", "1", "3", "2"]
+    },
+    {
+        "name": "Jeremiah Scanlon",
+        "photo": "https://picsum.photos/id/437/200/300",
+        "scores": ["5", "2", "2", "2", "4", "1", "3", "2", "5", "5"]
+    },
+    {
+        "name": "Louis T. Delia",
+        "photo": "https://picsum.photos/id/438/200/300",
+        "scores": ["3", "3", "4", "2", "2", "1", "3", "2", "2", "3"]
+    },
+    {
+        "name": "Lou Ritter",
+        "photo": "https://picsum.photos/id/439/200/300",
+        "scores": ["4", "3", "4", "1", "5", "2", "5", "3", "1", "4"]
+    },
+    {
+        "name": "Jordan Biason",
+        "photo": "https://picsum.photos/id/440/200/300",
+        "scores": ["4", "4", "2", "3", "2", "2", "3", "2", "4", "5"]
+    }];
+
+module.exports = friends;
