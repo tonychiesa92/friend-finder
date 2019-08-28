c3ecf1ddf4aa451f626f79971fc0c3a1e194bdd7
Tony C <tonychiesa92@gmail.com>
2019-08-28 19:15:59 -0400

    required epxress and path and created new instance of express, established port, and app.listen

---
+++ server.js
@@ -0,0 +1,17 @@
+let path = require("path");
+let express = require("express");
+
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+require("./app/routing/apiRoutes.js")(app);
+require("./app/routing/htmlRoutes.js")(app);
+
+
+app.listen(PORT, function(){
+    console.log("App listening on http://localhost:" + PORT);
+    
+});
